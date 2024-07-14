4eecd818f46c763edbad99d6f75245517bbf8882
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-07-14 06:01:12 +0300

    snap

--- src/pmo/models.py
+++ src/pmo/models.py
@@ -76,9 +76,6 @@ class BusinessUnit(CommonMixin, Base):
     parent = relationship("BusinessUnit", back_populates="children", remote_side=[id])
     children = relationship("BusinessUnit")
 
-    objectives: Mapped[List["Objective"]] = relationship(
-        back_populates="businessunit", cascade="all, delete-orphan"
-    )
     projects: Mapped[List["Project"]] = relationship(
         back_populates="businessunit", cascade="all, delete-orphan"
     )
@@ -107,14 +104,15 @@ class BusinessUnit(CommonMixin, Base):
                     w.register(g, edge=c)
             for r in p.risks:
                 r.register(g, edge=p)
-        for obj in self.objectives:
-            obj.register(g, edge=self)
-            for k in obj.keyresults:
-                k.register(g, edge=obj)
-                for ini in k.initiatives:
-                    ini.register(g, edge=k)
+
         for bp in self.businessplans:
             bp.register(g, edge=self)
+            for obj in bp.objectives:
+                obj.register(g, edge=bp)
+                for k in obj.keyresults:
+                    k.register(g, edge=obj)
+                    for ini in k.initiatives:
+                        ini.register(g, edge=k)
         g.render(directory='build', view=True)
 
 
@@ -124,7 +122,7 @@ class BusinessUnit(CommonMixin, Base):
 #     }
 
 # -----------------------------------------------------------------------------
-# Planning
+# Planning / OKR
 
 class BusinessPlan(CommonMixin, Base):
     __node_attr__ = { 'shape':'box', 'style': 'filled', 'fillcolor': 'lightyellow'}
@@ -132,10 +130,9 @@ class BusinessPlan(CommonMixin, Base):
     businessunit_id: Mapped[int] = mapped_column(ForeignKey("businessunit.id"))
     businessunit: Mapped["BusinessUnit"] = relationship(back_populates="businessplans")
 
-
-
-# -----------------------------------------------------------------------------
-# OKR
+    objectives: Mapped[List["Objective"]] = relationship(
+        back_populates="businessplan", cascade="all, delete-orphan"
+    )
 
 class Objective(CommonMixin, Base):
     """A significant, concrete, clearly defined goal.
@@ -144,8 +141,8 @@ class Objective(CommonMixin, Base):
     """
     __node_attr__ = { 'style': 'rounded,filled', 'shape':'box', 'fillcolor': 'aqua'}
 
-    businessunit_id: Mapped[int] = mapped_column(ForeignKey("businessunit.id"))
-    businessunit: Mapped["BusinessUnit"] = relationship(back_populates="objectives")
+    businessplan_id: Mapped[int] = mapped_column(ForeignKey("businessplan.id"))
+    businessplan: Mapped["BusinessPlan"] = relationship(back_populates="objectives")
     keyresults: Mapped[List["KeyResult"]] = relationship(
         back_populates="objective", cascade="all, delete-orphan"
     )
@@ -230,30 +227,3 @@ class Risk(CommonMixin, Base):
 
     project_id: Mapped[int] = mapped_column(ForeignKey("project.id"))
     project: Mapped["Project"] = relationship(back_populates="risks")
-
-
-
-
-if __name__ == '__main__':
-    DEBUG=True
-    if DEBUG:
-        engine = create_engine("sqlite://", echo=True)
-    else:
-        engine = create_engine("sqlite:///biz.db", echo=False)
-    Base.metadata.create_all(engine)
-    with Session(engine) as session:
-        bu = BusinessUnit(name="acme")
-        bp = BusinessPlan(name="3-year bizplan", businessunit=bu)
-        p1 = Project(name="Build gas-station", businessunit=bu)
-        ca1 = ControlAccount(name="Mobilization", project=p1)
-        r1 = Risk(name="schedule risk", project=p1)
-        wp1 = WorkPackage(name="Project management", controlaccount=ca1, start_date=date.today(), end_date=date.today())
-        ob1 = Objective(name="Crush the competition through acquisitions")
-        kr1 = KeyResult(name="Acquire 3 small players in our industry")
-        in1 = Initiative(name="Secure M&A financing approval from banks")
-        bu.objectives = [ob1]
-        kr1.initiatives = [in1]
-        ob1.keyresults = [kr1]
-        session.add_all([bu, ob1, kr1, in1])
-        session.commit()
-        bu.mk_graph()
--- tests/test_pmo.py
+++ tests/test_pmo.py
@@ -20,16 +20,13 @@ def test_models():
     with Session(engine) as session:
         bu = BusinessUnit(name="acme")
         bp = BusinessPlan(name="3-year bizplan", businessunit=bu)
+        ob1 = Objective(name="Crush the competition through acquisitions", businessplan=bp)
+        kr1 = KeyResult(name="Acquire 3 small players in our industry", objective=ob1)
+        in1 = Initiative(name="Secure M&A financing approval from banks", keyresult=kr1)
         p1 = Project(name="Build gas-station", businessunit=bu)
         ca1 = ControlAccount(name="Mobilization", project=p1)
         r1 = Risk(name="schedule risk", project=p1)
         wp1 = WorkPackage(name="Project management", controlaccount=ca1, start_date=date.today(), end_date=date.today())
-        ob1 = Objective(name="Crush the competition through acquisitions")
-        kr1 = KeyResult(name="Acquire 3 small players in our industry")
-        in1 = Initiative(name="Secure M&A financing approval from banks")
-        bu.objectives = [ob1]
-        kr1.initiatives = [in1]
-        ob1.keyresults = [kr1]
-        session.add_all([bu, ob1, kr1, in1])
+        session.add_all([bu, bp, r1])
         session.commit()
         bu.mk_graph()
